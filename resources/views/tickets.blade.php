f1276059025c3031c9b0f472abe93a7f674c94da
sofiyek <lolitaitamov@gmail.com>
2024-06-08 14:54:02 +0300

    tickets style

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -2,6 +2,9 @@
 
 @section('title', 'tickets')
 
+<link href="{{ asset('css/tickets.css?v=') . time() }}" rel="stylesheet">
+<link href="{{ asset('css/image-cover.css?v=') . time() }}" rel="stylesheet">
+
 @section('content')
 
     <div class="content">
@@ -13,6 +16,9 @@
                 @foreach ($tickets as $ticket)
                     <div class="ticket">
                         <h2>{{ $ticket->exhibition->name }}</h2>
+                        <div class="cover">
+                            <img src="{{ Storage::url($ticket->exhibition->photo) }}" alt="Photo of the exhibition">
+                        </div>
                         <p>Количество билетов: {{ $ticket->quantity }}</p>
                         <p>Дата покупки: {{ $ticket->created_at }}</p>
                     </div>
