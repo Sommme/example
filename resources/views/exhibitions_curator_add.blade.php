c5b1cc9b0ff86a993e0a6853863073dfe41c454f
Sommme <anastasia_ol_anastasia@mail.ru>
2024-06-08 10:46:27 +0300

    exhibitions_curator_add form

--- resources/views/exhibitions_curator_add.blade.php
+++ resources/views/exhibitions_curator_add.blade.php
@@ -2,24 +2,65 @@
 
 @section('title', 'Add Exhibition')
 <link href="{{ asset('css/custom-btn.css?v=') . time() }}" rel="stylesheet">
+<link href="{{ asset('css/form.css?v=') . time() }}" rel="stylesheet">
 @section('content')
+    <div class="content">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
     <h1>Add Exhibition</h1>
     <form action="{{url('/new_exhibition/create')}}" method="POST" enctype="multipart/form-data">
         @csrf
-        <label for="name">Name:</label><br>
-        <input type="text" id="name" name="name"><br>
 
-        <label for="address">Address:</label><br>
-        <input type="text" id="address" name="address"><br>
+        <div class = "form-title">
+            <h3>New Exhibition</h3>
+        </div>
 
-        <label for="ticket_price">Ticket Price:</label><br>
-        <input type="number" id="ticket_price" name="ticket_price"><br>
 
-        <label for="max_tickets">Max Tickets:</label><br>
-        <input type="number" id="max_tickets" name="max_tickets"><br>
+        <br>
+         <div class = "input-block">
+            <label for="name">Name:</label><br>
+            <input type="text" id="name" name="name"><br>
+            @error('name')
+                <p class="error-text">{{ $message }}</p>
+            @enderror
+        </div>
 
-        <label for="photo">Photo:</label><br>
-        <input type="file" id="photo" name="photo"><br>
+
+        <div class="input-block">
+            <label for="address">Address:</label><br>
+            <input type="text" id="address" name="address"><br>
+            @error('address')
+                <p class="error-text">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="input-block">
+            <label for="ticket_price">Ticket Price:</label><br>
+            <input type="number" id="ticket_price" name="ticket_price"><br>
+            @error('ticket_price')
+                <p class="error-text">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="input-block">
+            <label for="max_tickets">Max Tickets:</label><br>
+            <input type="number" id="max_tickets" name="max_tickets"><br>
+            @error('max_tickets')
+                <p class="error-text">{{ $message }}</p>
+            @enderror
+        </div>
+
+        <div class="input-block">
+            <label for="photo">Photo</label>
+            <input type="file" id="photo" name="photo" accept=".png, .jpg, .jpeg" required>
+            @error('photo')
+                <p class="error-text">{{ $message }}</p>
+            @enderror
+        </div>
 
         <label for="direction">Direction</label><br>
         <select name="direction" id="direction">
@@ -28,12 +69,16 @@
             @endforeach
         </select><br><br>
 
+
+        <div class="input-block">
         <label for="exhibits">Exhibits</label><br>
         <select name="exhibits[]" id="exhibits" multiple>
             @foreach ($exhibits as $exhbit)
                 <option value="{{$exhbit['id']}}">{{$exhbit['name']}}</option>
             @endforeach
         </select><br><br>
+
+        </div>
 
         <div id="dates">
             <div class="date">
@@ -46,5 +91,6 @@
         </div>
 
         <button type="submit">Add</button>
+    </div>
     </form>
 @endsection
